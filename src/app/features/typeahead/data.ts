c610bcab6ceea8af93ebe510689d17cfcfcc9786
hi-shekhar <himanshu26592@gmail.com>
2025-07-06 18:50:55 +0200

    type ahead with keyboard

---
+++ src/app/features/typeahead/data.ts
@@ -0,0 +1,5 @@
+export const countries: string[] = [
+    'United States', 'Canada', 'Mexico', 'Brazil', 'Argentina',
+    'United Kingdom', 'France', 'Germany', 'Italy', 'Spain',
+    'China', 'India', 'Japan', 'Australia', 'South Africa'
+];
